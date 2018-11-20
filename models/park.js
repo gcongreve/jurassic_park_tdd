bbe6a0c498532e4a4a644d382193aa65015c2d6f
Graeme Congreve <junkmailgmc@gmail.com>
2018-11-20 17:52:48 +0000

    finished all but 1 park tests

--- models/park.js
+++ models/park.js
@@ -2,7 +2,47 @@ const Park = function (name) {
 
   this.name = name;
   this.ticketPrice = 50;
-
+  this.dinosaurs = [];
 }
+
+Park.prototype.addDinosaur = function (dinosaur) {
+  this.dinosaurs.push(dinosaur);
+};
+
+
+Park.prototype.removeDinosaur = function (dinosaur) {
+ for (let i = 0; i < this.dinosaurs.length; i++) {
+   if (this.dinosaurs[i] === dinosaur){
+     this.dinosaurs.splice(i, 1);
+     break;
+   }
+ }
+};
+
+Park.prototype.mostPopularDinosaur = function () {
+  let mostPopular = "";
+  let visitors = 0;
+  for (const dino of this.dinosaurs){
+    if (dino.guestsAttractedPerDay > visitors){
+      visitors = dino.guestsAttractedPerDay;
+      mostPopular = dino;
+    }
+  }
+  return mostPopular;
+};
+
+
+Park.prototype.allDinosaursSpecies = function (species) {
+  let dinoSpecies = [];
+  for (const dino of this.dinosaurs){
+    if (dino.species === species){
+      dinoSpecies.push(dino);
+    }
+  }
+  return dinoSpecies;
+};
+
+
+
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,9 +5,15 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 
   let park;
+  let dinosaur;
+  let dinosaur2;
 
   beforeEach(function () {
-    park = new Park('Jurassic Park')
+    park = new Park('Jurassic Park');
+    dinosaur = new Dinosaur('T Rex', 'carnivore', 100);
+    dinosaur3 = new Dinosaur('T Rex', 'carnivore', 100);
+    dinosaur2 = new Dinosaur('Stegathingy', 'herbivore', 101);
+
   })
 
   it('should have a name', function () {
@@ -20,15 +26,39 @@ describe('Park', function() {
     assert.strictEqual(actual, 50)
   });
 
-  it('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function () {
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, [])
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function () {
+    park.addDinosaur(dinosaur);
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 1)
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function () {
+    park.addDinosaur(dinosaur);
+    park.addDinosaur(dinosaur2);
+    park.removeDinosaur(dinosaur2);
+    const actual = park.dinosaurs[0].species
+    assert.strictEqual(actual, 'T Rex')
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function () {
+    park.addDinosaur(dinosaur);
+    park.addDinosaur(dinosaur2);
+    const actual = park.mostPopularDinosaur();
+    assert.strictEqual(actual, dinosaur2)
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to find all dinosaurs of a particular species', function () {
+    park.addDinosaur(dinosaur);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = park.allDinosaursSpecies('T Rex').length
+    assert.strictEqual(actual, 2)
+  });
 
   it('should be able to remove all dinosaurs of a particular species');
 
